cab6263d7a77f89e87a4ae941033b968599a5c4d
melody-story <rnlqls7454@naver.com>
2023-03-29 20:47:46 +0900

    ADD) GET /members/{id} - Member read

---
+++ src/main/java/com/example/sns/domain/member/service/MemberReadService.java
@@ -0,0 +1,22 @@
+package com.example.sns.domain.member.service;
+
+import com.example.sns.domain.member.dto.MemberDto;
+import com.example.sns.domain.member.entity.Member;
+import com.example.sns.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberReadService {
+    private final MemberRepository memberRepository;
+
+    public MemberDto getMember(Long id) {
+        return toDto(memberRepository.findById(id).orElseThrow());
+    }
+
+    public MemberDto toDto(Member member) {
+        return new MemberDto(member.getId(), member.getEmail(),member.getNickname(), member.getBirthday() )
+    }
+}
