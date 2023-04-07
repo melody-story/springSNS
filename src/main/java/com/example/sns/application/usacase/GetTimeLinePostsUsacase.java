99c76b0a8917c9361f48f42126d19a03e7b0b13b
melody-story <rnlqls7454@naver.com>
2023-04-07 18:08:40 +0900

    TimeLine 구현 onging -  Fan out TimeLine

--- src/main/java/com/example/sns/application/usacase/GetTimeLinePostsUsacase.java
+++ src/main/java/com/example/sns/application/usacase/GetTimeLinePostsUsacase.java
@@ -19,14 +19,6 @@ public class GetTimeLinePostsUsacase {
         1. Post 와 Follow 도메인 두가지 필요
         2. 무한 스크롤 방식인 Cursor 기변 페이징 구현
 
-        순서
-        1. memberId로 Follow 조회
-        2. 1번의 결과로 게시물 조회
-
-       시간 복잡도
-       log(Follow 전체 레코드) + 해당회원의 Following 회원수  * log(Post 전체 레코드)
-
-       Fan Out On Read (Pull model) : 조회 시점에 부화가 발생
        Fan Out On Write (Push model) : 쓰기 시점에 부화가 발생
      */
 
@@ -34,6 +26,27 @@ public class GetTimeLinePostsUsacase {
     private final FollowReadService followReadService;
 
     public PageCursor<Post> execute(Long memberId, CursorRequest cursorRequest) {
+       /*
+            순서
+            1. memberId로 Follow 조회
+            2. 1번의 결과로 게시물 조회
+
+           시간 복잡도
+           log(Follow 전체 레코드) + 해당회원의 Following 회원수  * log(Post 전체 레코드)
+
+            Fan Out On Read (Pull model) : 조회 시점에 부화가 발생
+        */
+        List<Follow> followings = followReadService.getFollowings(memberId);
+        List<Long> followingMemberIds = followings.stream().map(Follow::getToMemberId).toList();
+        return postReadService.getPosts(followingMemberIds, cursorRequest);
+    }
+
+    public PageCursor<Post> executeByTimeline(Long memberId, CursorRequest cursorRequest) {
+        /*
+            순서
+            1. Timeline 테이블 조회
+            2. 1번에 해당하는 게시물을 조회
+         */
         List<Follow> followings = followReadService.getFollowings(memberId);
         List<Long> followingMemberIds = followings.stream().map(Follow::getToMemberId).toList();
         return postReadService.getPosts(followingMemberIds, cursorRequest);
